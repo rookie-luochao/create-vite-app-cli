72d21c9118e9c360719452c27a66d18b6e6079b7
luochao <1055120207@qq.com>
2023-11-27 23:00:32 +0800

    feat: 修改项目模板prettier格式化配置

--- template-vue-ts/components.d.ts
+++ template-vue-ts/components.d.ts
@@ -5,15 +5,15 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module "vue" {
+declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import("element-plus/es")["ElButton"];
-    ElConfigProvider: typeof import("element-plus/es")["ElConfigProvider"];
-    ElForm: typeof import("element-plus/es")["ElForm"];
-    ElFormItem: typeof import("element-plus/es")["ElFormItem"];
-    ElInput: typeof import("element-plus/es")["ElInput"];
-    HelloWorld: typeof import("./src/components/HelloWorld.vue")["default"];
-    RouterLink: typeof import("vue-router")["RouterLink"];
-    RouterView: typeof import("vue-router")["RouterView"];
+    ElButton: typeof import('element-plus/es')['ElButton'];
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider'];
+    ElForm: typeof import('element-plus/es')['ElForm'];
+    ElFormItem: typeof import('element-plus/es')['ElFormItem'];
+    ElInput: typeof import('element-plus/es')['ElInput'];
+    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default'];
+    RouterLink: typeof import('vue-router')['RouterLink'];
+    RouterView: typeof import('vue-router')['RouterView'];
   }
 }
